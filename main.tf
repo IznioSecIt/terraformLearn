8109c291e5927c90d1eb748f4528d9f039fdff92
Iznio <aurelien_etienne@hotmail.com>
2026-02-26 07:44:37 +0100

    end of chapter01

--- main.tf
+++ main.tf
@@ -44,6 +44,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
   from_port         = var.server_port
   ip_protocol       = "tcp"
   to_port           = var.server_port
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_instance" {
+  security_group_id = aws_security_group.instance.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_instance" "example" {
@@ -159,6 +166,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_alb" {
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_alb" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_lb_target_group" "asg" {
