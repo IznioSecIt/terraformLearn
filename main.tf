6f9066296eac9f9ae8eaa831aaa52baf07bdc33a
Iznio <aurelien_etienne@hotmail.com>
2026-02-26 07:44:16 +0100

    end chapter01

--- main.tf
+++ main.tf
@@ -24,6 +24,11 @@ output "public_ip" {
   description = "The public IP address of the web server"
 }
 
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   description = "Allow access to webserver"
@@ -82,6 +87,9 @@ resource "aws_autoscaling_group" "example" {
 
   vpc_zone_identifier = data.aws_subnets.default.ids
 
+  target_group_arns = [ aws_lb_target_group.asg.arn ]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 5
 
@@ -93,5 +101,79 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [ aws_security_group.alb.id ]
+
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  description = "Allow access to ALB"
+
+  tags = {
+    Name = "allow_ALB_access"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_alb" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
   }
 }
